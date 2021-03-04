fe731d226526dd7eaafc022190fa857e32ae4857
Randolph Hwang <dearhusky@yonsei.ac.kr>
2021-03-04 23:15:32 +0900

    Version 2.1.0
     Array(배열) 과 Loop(반복문)을 추가했습니다. 약간 어렵기도 하고, 코드가 오히려 더 길어진 감이 없잖아 있지만 나중에 dividend가 많아진다고 가정할 때 배열 부분의 코드에만 새로운 dividendrate 값을 정해주면 되는 일이 된 것 같습니다.
    
     다만 dividendRates에도 배열의 갯수와, 각 순서에 해당하는 수익 분배율을 정해줘야 하기때문에 코드를 수정할 때 번거로운 점이 있습니다. dividend가 모두 1/n으로 수익을 나눈다고 가정한다면 코드가 더 짧아지고, 배열의 갯수만 늘리면 되는 장점이 있습니다. 그러나 현실에서는 그렇게 단순하게 1/n을 할 것 같진 않기에.. 사람이 한 명 늘어날 때 마다 정말 코드를 한 줄씩 추가하는 방법밖엔 없는 것인지, 한번 고민해볼 필요가 있는 것 같습니다.

---
+++ MyAccountingApp3.java
@@ -0,0 +1,36 @@
+import javax.swing.JOptionPane;
+
+public class MyAccountingApp3 {
+
+	public static void main(String[] args) {
+		
+		String id = JOptionPane.showInputDialog("Your product is : ");
+		double  VOS = Double.parseDouble(JOptionPane.showInputDialog("Enter your VOS(Value Of Supply) : ")); // VOS = Value Of Supply
+		double vatRate = 0.1;
+		double expenseRate = 0.3;
+		double VAT = VOS * vatRate;
+		double Total = VOS + VAT;
+		double Expense = VOS * expenseRate;
+		double Income = VOS - Expense;
+		
+		double [] dividendRates = new double [3];
+		dividendRates[0] = 0.5;
+		dividendRates[1] = 0.3;
+		dividendRates[2] = 0.2;
+		
+		
+		System.out.println("Your Product : "+id);
+		System.out.println("Value of supply : "+VOS);
+		System.out.println("VAT : " +VAT);
+		System.out.println("Total : " +Total);
+		System.out.println("Expense : "+Expense);
+		System.out.println("Income : "+Income);
+
+		int i = 0;
+		while(i < dividendRates.length) {
+			System.out.println("Dividend " + (i+1) +" : " + (Income * dividendRates [i]));
+			i = i + 1;
+			}
+		
+		}
+}
